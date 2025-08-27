b217c52d65ac91d2961b09f611030d357a6a9a24
Paul Berruti <pberruti@google.com>
2025-08-27 00:24:43 +0000

    Refactor

--- pkg/webhook/config_test.go
+++ pkg/webhook/config_test.go
@@ -40,112 +40,68 @@ func TestConfig_Validate(t *testing.T) {
 	t.Parallel()
 
 	cases := []struct {
-		name   string
-		cfg    *Config
-		expErr string
+		name    string
+		mutator func(c *Config)
+		expErr  string
 	}{
 		{
-			name: "valid",
-			cfg:  generateValidConfig(),
+			name:    "valid",
+			mutator: nil,
 		},
 		{
-			name: "invalid_runner_label",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.RunnerLabel = ""
-				return c
-			}(),
-			expErr: "RUNNER_LABEL is required",
+			name:    "invalid_runner_label",
+			mutator: func(c *Config) { c.RunnerLabel = "" },
+			expErr:  "RUNNER_LABEL is required",
 		},
 		{
-			name: "invalid_environment",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.Environment = "invalid"
-				return c
-			}(),
-			expErr: `ENVIRONMENT must be one of 'production' or 'autopush', got "invalid"`,
+			name:    "invalid_environment",
+			mutator: func(c *Config) { c.Environment = "invalid" },
+			expErr:  `ENVIRONMENT must be one of 'production' or 'autopush', got "invalid"`,
 		},
 		{
-			name: "missing_github_app_id",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.GitHubAppID = ""
-				return c
-			}(),
-			expErr: "GITHUB_APP_ID is required",
+			name:    "missing_github_app_id",
+			mutator: func(c *Config) { c.GitHubAppID = "" },
+			expErr:  "GITHUB_APP_ID is required",
 		},
 		{
-			name: "missing_webhook_key_mount_path",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.GitHubWebhookKeyMountPath = ""
-				return c
-			}(),
-			expErr: "WEBHOOK_KEY_MOUNT_PATH is required",
+			name:    "missing_webhook_key_mount_path",
+			mutator: func(c *Config) { c.GitHubWebhookKeyMountPath = "" },
+			expErr:  "WEBHOOK_KEY_MOUNT_PATH is required",
 		},
 		{
-			name: "missing_webhook_key_name",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.GitHubWebhookKeyName = ""
-				return c
-			}(),
-			expErr: "WEBHOOK_KEY_NAME is required",
+			name:    "missing_webhook_key_name",
+			mutator: func(c *Config) { c.GitHubWebhookKeyName = "" },
+			expErr:  "WEBHOOK_KEY_NAME is required",
 		},
 		{
-			name: "missing_kms_app_private_key_id",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.KMSAppPrivateKeyID = ""
-				return c
-			}(),
-			expErr: "KMS_APP_PRIVATE_KEY_ID is required",
+			name:    "missing_kms_app_private_key_id",
+			mutator: func(c *Config) { c.KMSAppPrivateKeyID = "" },
+			expErr:  "KMS_APP_PRIVATE_KEY_ID is required",
 		},
 		{
-			name: "missing_runner_location",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.RunnerLocation = ""
-				return c
-			}(),
-			expErr: "RUNNER_LOCATION is required",
+			name:    "missing_runner_location",
+			mutator: func(c *Config) { c.RunnerLocation = "" },
+			expErr:  "RUNNER_LOCATION is required",
 		},
 		{
-			name: "missing_runner_project_id",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.RunnerProjectID = ""
-				return c
-			}(),
-			expErr: "RUNNER_PROJECT_ID is required",
+			name:    "missing_runner_project_id",
+			mutator: func(c *Config) { c.RunnerProjectID = "" },
+			expErr:  "RUNNER_PROJECT_ID is required",
 		},
 		{
-			name: "missing_runner_repository_id",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.RunnerRepositoryID = ""
-				return c
-			}(),
-			expErr: "RUNNER_REPOSITORY_ID is required",
+			name:    "missing_runner_repository_id",
+			mutator: func(c *Config) { c.RunnerRepositoryID = "" },
+			expErr:  "RUNNER_REPOSITORY_ID is required",
 		},
 		{
-			name: "missing_runner_service_account",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.RunnerServiceAccount = ""
-				return c
-			}(),
-			expErr: "RUNNER_SERVICE_ACCOUNT is required",
+			name:    "missing_runner_service_account",
+			mutator: func(c *Config) { c.RunnerServiceAccount = "" },
+			expErr:  "RUNNER_SERVICE_ACCOUNT is required",
 		},
 		{
-			name: "invalid_extra_runner_count",
-			cfg: func() *Config {
-				c := generateValidConfig()
-				c.ExtraRunnerCount = "abc"
-				return c
-			}(),
-			expErr: "EXTRA_RUNNER_COUNT must be an integer",
+			name:    "invalid_extra_runner_count",
+			mutator: func(c *Config) { c.ExtraRunnerCount = "abc" },
+			expErr:  "EXTRA_RUNNER_COUNT must be an integer",
 		},
 	}
 
@@ -153,7 +109,12 @@ func TestConfig_Validate(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			err := tc.cfg.Validate()
+			cfg := generateValidConfig()
+			if tc.mutator != nil {
+				tc.mutator(cfg)
+			}
+
+			err := cfg.Validate()
 			if diff := testutil.DiffErrString(err, tc.expErr); diff != "" {
 				t.Fatal(diff)
 			}
